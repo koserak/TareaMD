b8cf01e80433b02cf83e7a60caee7a807a61fa57
FurioStyle <benjamin.bglh@gmail.com>
2026-05-11 18:26:03 -0400

    agregadas definiciones extra

--- estructuras.h
+++ estructuras.h
@@ -10,13 +10,26 @@
 #define MAX_NOMBRE      64
 #define MAX_TURISTICOS  100
 #define EPSILON         1e-6
+#define INF 1e30
+
+// Direcciones
+typedef enum {
+    DIR_RECTO,
+    DIR_IZQUIERDA,
+    DIR_DERECHA,
+    DIR_MEDIA_VUELTA
+} Direccion;
+
+// Struct para Calle
 
 typedef struct {
     char   nombre[MAX_NOMBRE];
     double x0, y0;   /* inicio */
     double x1, y1;   /* fin    */
-    char   eje;      /* 'X' o 'Y' */
+    char   eje;      /* 'X', 'Y', 'D'  */
 } Calle;
+
+// Struct para Vertices (nodos)
 
 typedef struct {
     double x, y;
@@ -25,6 +38,7 @@ typedef struct {
     int num_turisticos;   /* indice en arreglo de turisticos, -1 si no aplica */
 } Vertice;
 
+// Struct para almacenar los puntos turisticos.
 typedef struct {
     char   nombre[MAX_NOMBRE];
     char   calle[MAX_NOMBRE];
@@ -33,16 +47,22 @@ typedef struct {
     int    visitado;
 } PuntoTuristico;
 
+// Struct para modelar las Aristas
+
 typedef struct Arista {
     int           destino;
     struct Arista *sig;
 } Arista;
+
+// Struct para modelar el grafo
 
 typedef struct {
     Vertice  Vertices[MAX_VERTICES];
     Arista  *adyacencia[MAX_VERTICES];
     int      num_Vertices;
 } Grafo;
+
+// Struct para modelar la Ciudad
 
 typedef struct {
     Calle          calles[MAX_CALLES];
@@ -52,7 +72,7 @@ typedef struct {
     Grafo          grafo;
 } Ciudad;
 
-/* Calcula distancia euclidiana entre dos vertices */
+// Calcula la distancia entre dos vertices
 static inline double calcular_distancia(Vertice a, Vertice b) {
     double dx = a.x - b.x;
     double dy = a.y - b.y;
